a4637d1b58ed7b38919d25d3b039387238c96183
Daren Schaad and Neil Larion <neil.larion@gmail.com>
2016-01-13 14:07:42 -0800

    fix the income logic and add input success notification logic

--- js/scripts.js
+++ js/scripts.js
@@ -1,4 +1,16 @@
 $(function(){
+  $('#income').keyup(function(){
+    var income = $("#income").val();
+    console.log(isNaN(income));
+    if(isNaN(income)){
+      $('.form-group').removeClass('has-success');
+      $('.form-group').addClass('has-error');
+    } else {
+      $('.form-group').removeClass('has-error');
+      $('.form-group').addClass('has-success');
+    }
+  });
+
   $(".container form").submit(function(event){
     var income = $("#income").val(),
     trans = $("input[name=optionsRadios]:checked").val(),
@@ -6,15 +18,15 @@ $(function(){
     donate = $("#donate").is(':checked');
     var obj = {};
     obj.gun = gun;
-    console.log(obj);
     obj.donate = donate;
+    console.log(obj);
     $(".hillary").hide();
     $(".bernie").hide();
     $(".donald").hide();
     $(".ted").hide();
     $(".alert").hide();
 
-    if(income){
+    if(income && !isNaN(income)){
       if (income >999999){
         if (gun){
           if(donate){
